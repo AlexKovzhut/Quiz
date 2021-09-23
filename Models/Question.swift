6786d5b89a8ea6de5f42f1606c813b74c669803f
Alexander Kovzhut <akovzhut@icloud.com>
2021-09-24 00:27:00 +0300

    Commit

--- Models/Question.swift
+++ Models/Question.swift
@@ -28,24 +28,40 @@ struct Question {
                 Answer(title: "I'm just trying to find my place in this world, but in the company of like-minded people it will work out faster", type: .mysterious),
                 Answer(title: "Superpowers that no one else has, and indeed I'm cool - of course, I was called", type: .arrogant)
             ]),
-            Question(
-                title: "Do you find common language with other people easily?",
-                type: .single,
-                answers: [
-                Answer(title: "It's not so easy for me to become the soul of the company, usually I have 1-2 friends, but very close ones.", type: .reluctant),
-                Answer(title: "The motivation of other people, their emotions and thoughts is important for me. If a person evokes sympathy or respect, I will listen to him and help him.", type: .friend),
-                Answer(title: "I am very witty, but for some reason not everyone has fun when I joke. Maybe I hurt them somehow? Ahh, don't give a damn!", type: .mysterious)
-            ]),
             Question(title: "What techniques do you usually use?", type: .multiple, answers: [
                 Answer(title: "Fighting", type: .friend),
                 Answer(title: "Technologies", type: .arrogant),
                 Answer(title: "Stealth", type: .mysterious),
                 Answer(title: "Magic", type: .reluctant)
             ]),
-            Question(title: "Which Side Are You On?", type: .ranged, answers: [
-                Answer(title: "Evil", type: .mysterious),
-                Answer(title: "Neutral", type: .arrogant),
-                Answer(title: "Good", type: .friend)
+            Question(
+                title: "Do you find common language with other people easily?",
+                type: .single,
+                answers: [
+                Answer(title: "It's not so easy for me to become the soul of the company, usually I have 1-2 friends, but very close ones", type: .reluctant),
+                Answer(title: "The motivation of other people, their emotions and thoughts is important for me. If a person evokes sympathy or respect, I will listen to him and help him", type: .friend),
+                Answer(title: "I am very witty, but for some reason not everyone has fun when I joke. Maybe I hurt them somehow? Ahh, don't give a damn!", type: .mysterious)
+            ]),
+            Question(
+                title: "What are you ready for for a friend?",
+                type: .single,
+                answers: [
+                Answer(title: "In general, it is difficult for me to put myself in the place of another, after all, we are all selfish and think only of ourselves", type: .mysterious),
+                Answer(title: "I may seem like a loner, but I'm willing to do a lot for my friends", type: .reluctant),
+                Answer(title: "I see no point in being a hero if you don't help others. And friends are sacred!", type: .friend)
+            ]),
+            Question(
+                title: "Your superhero colleague has lost his temper and is possibly a threat. Your actions?",
+                type: .single,
+                answers: [
+                Answer(title: "No need to understand. Out in the cold - that's all. Maybe you should even kill him", type: .mysterious),
+                Answer(title: "I will try to talk to him, find out if everything is in order, maybe something happened", type: .friend),
+                Answer(title: "So, bring the best equipment of our technogenesis, we need to scan the brain and find out what is wrong with it", type: .arrogant)
+            ]),
+            Question(title: "And last question...which side are you on?", type: .ranged, answers: [
+                Answer(title: "Dark", type: .mysterious),
+                Answer(title: "Center", type: .arrogant),
+                Answer(title: "Light", type: .friend)
             ]),
         ]
     }
